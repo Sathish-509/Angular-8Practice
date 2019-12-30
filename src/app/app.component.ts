f9bedd4ee7e68549a91dc965a159b6f4dbeef65d
Sathish <sathish9yerramsetti@gmail.com>
2019-12-30 23:58:24 +0800

    directives pactice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Sathish';
+  flag = true;
+  studentsList = ["Sathish", "Virat"];
+  currentName = "";
+
+  toggleFlag() {
+    this.flag = !this.flag;
+    return this.flag;
+  }
+
+  getColor() {
+    if(this.flag) {
+      return "green";
+    } else{
+      return "red";
+    }
+  }
+
+  addStudent() {
+    this.studentsList.push(this.currentName);
+  }
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+    ServerName = "HostMachine";
+    ServerPid = "173.0.0.1";
+    ServerStatus = "Active";
+    statusFlag = true;
+    buttonState = true;
+
+    constructor() {
+        setTimeout(() => {
+            this.buttonState = false;
+        }, 2500);
+    }
+
+    toggleServerStatus() {
+        this.statusFlag = !this.statusFlag;
+        if(this.statusFlag) {
+            this.ServerStatus = "Active";
+        } else {
+            this.ServerStatus = "InActive";
+        }
+        return this.ServerStatus;
+    }
 }
